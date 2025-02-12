4e6801f15e3065822735e7329fc2402bbe4a6f90
shiyan224 <shiyan0224@outlook.com>
2025-02-12 19:59:11 +0800

    nothing

--- lacam2/src/planner.cpp
+++ lacam2/src/planner.cpp
@@ -129,7 +129,8 @@ Solution Planner::solve(std::string& additional_info)
       H_goal = H;
       solver_info(1, "found solution, cost: ", H->g);
       //if (objective == OBJ_NONE) break;
-      continue;
+//      continue;
+      break;
     }
 
     // create successors at the low-level search, BFS
